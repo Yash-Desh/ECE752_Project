c6140ac778e01f162d25abfff88dc07c4f96a092
Yash Deshpande <23yashd@gmail.com>
2025-04-02 15:09:10 -0500

    625_x264 added to test programs

---
+++ Test_Programs/625_x264/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###### Parameters #####
+pin_tool=dcache
+benchmark=625_x264
+# No data/all folder
+#######################
+
+cd ../../Intel_Pin/source/tools/Memory/obj-intel64
+echo "Deleting Object Files from obj-intel64 folder"
+rm $pin_tool.so $pin_tool.o
+cd ..
+echo "Calling make in Memory Folder"
+make
+cd ../../../../Test_Programs/$benchmark/
+echo "Executing Pin Progam" 
+../../Intel_Pin/pin -t ../../Intel_Pin/source/tools/Memory/obj-intel64/$pin_tool.so -- ./ldecod_s_base.mytest-m64 -i BuckBunny.264 -o BuckBunny.yuv > $benchmark.log
